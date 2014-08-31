33a33b5d22fd0388d1ce781718e839a30c2eb59f
Steve Lacy <me@slacy.me>
2014-08-31 12:12:12 -0700

    add files

--- model.js
+++ model.js
@@ -2,18 +2,34 @@ var ampersandModel = require('ampersand-model');
 
 
 var Model =  ampersandModel.extend({
-  url: 'http://echo.jsontest.com/content/data/name/first'
+  url: 'http://echo.jsontest.com/content/data/name/first',
   props: {
     content: 'string',
     name: 'string'
   }
 });
 
+$el = document.getElementById('content');
 
 model = new Model();
-
-model.fetch()
-
-model.on('fetch', function(data){
-  console.log(this);
+model.fetch({
+  success: function(data){
+    $el.innerHTML = data._values.name;
+  },
+  error: function(data) {
+    $el.innerHTML = 'error';
+  }
 });
+
+
+
+model.on('sync', function(data){
+  node = document.createElement('div');
+  innerNode = document.createTextNode(data._values.name);
+  node.appendChild(innerNode);
+  $el.appendChild(node);
+});
+
+setInterval(function(){
+  model.fetch();
+},2000);
